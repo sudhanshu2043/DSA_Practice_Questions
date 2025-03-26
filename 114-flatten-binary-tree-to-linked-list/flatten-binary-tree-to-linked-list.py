1eddd8c65ac2a089cd373b3a675244655fdd926f
sudhanshu2043 <65844744+sudhanshu2043@users.noreply.github.com>
2025-03-26 17:28:24 +0530

    Time: 1 ms (16.83%) | Memory: 18 MB (72.04%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.py
@@ -5,25 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self,root,inorder):
-        if root==None:
-            return
-        inorder.append(root.val)
-        self.dfs(root.left,inorder)
-        self.dfs(root.right,inorder)
-
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
         Do not return anything, modify root in-place instead.
         """
         if root is None:
             return
-        inorder=[]
-        self.dfs(root,inorder)
-        temp = root
-        temp.val = inorder[0]  # Set root value
-        temp.left = None
-        for i in range(1,len(inorder)):
-            temp.right=TreeNode(inorder[i])
-            temp=temp.right
+        curr=root
+        while curr!=None:
+            if curr.left!=None:
+                prev=curr.left
+                while prev.right!=None:
+                    prev=prev.right
+                prev.right=curr.right
+                curr.right=curr.left
+                curr.left=None
+            curr=curr.right
+            
+
+        return root
         
