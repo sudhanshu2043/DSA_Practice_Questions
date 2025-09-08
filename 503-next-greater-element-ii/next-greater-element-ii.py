791c5348f8a5360bc80169dcbfb83d73de206472
sudhanshu2043 <65844744+sudhanshu2043@users.noreply.github.com>
2025-09-08 23:11:57 +0530

    Time: 15 ms (93.87%) | Memory: 19.5 MB (61.84%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,11 +1,9 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [-1] * n  # Initialize all results as -1
-        st = []  # Stack to keep track of indices
-
-        # Traverse the array twice to handle the circular nature
-        for i in range(2 * n):
+        n=len(nums)
+        ans=[-1]*n
+        st=[]
+        for i in range(2*n):
             num = nums[i % n]  # Get the current element, modulo to handle circular
             while st and nums[st[-1]] < num:
                 ans[st.pop()] = num
