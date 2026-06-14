aee3ae9f64cf78198613f5381b85a0b6aaeee226
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-14 20:09:05 +0530

    Time: 145 ms (53.54%) | Memory: 79.1 MB (27.68%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,93 +1,60 @@
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.freq = 1
-        self.prev = None
-        self.next = None
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = Node(None, None)  # Dummy head
-        self.tail = Node(None, None)  # Dummy tail
-        self.head.next = self.tail
-        self.tail.prev = self.head
-    
-    def add_node(self, node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
-    
-    def remove_node(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
-    
-    def remove_last(self):
-        if self.head.next == self.tail:
-            return None
-        last_node = self.tail.prev
-        self.remove_node(last_node)
-        return last_node
+from collections import defaultdict, OrderedDict
 
 class LFUCache:
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.size = 0
+        self.cap = capacity
         self.min_freq = 0
-        self.node_map = {}  # key -> Node
-        self.freq_map = {}  # freq -> DoublyLinkedList
-    
-    def _update_freq(self, node):
-        freq = node.freq
-        self.freq_map[freq].remove_node(node)
+        # key -> (value, frequency)
+        self.key_map = {}
+        # frequency -> OrderedDict(key -> True)
+        # OrderedDict maintains insertion order, behaving like a Doubly Linked List
+        self.freq_map = defaultdict(OrderedDict)
+
+    def _update_frequency(self, key: int, value: int = None) -> int:
+        """Helper to advance a key's frequency and move its position in O(1)"""
+        curr_val, freq = self.key_map[key]
+        # If a new value is provided (from a put operation), update it
+        if value is not None:
+            curr_val = value
+        # 1. Remove the key from its current frequency bucket
+        del self.freq_map[freq][key]
         
-        if not self.freq_map[freq].head.next != self.freq_map[freq].tail:
-            if self.min_freq == freq:
-                self.min_freq += 1
-        
-        node.freq += 1
-        new_freq = node.freq
-        
-        if new_freq not in self.freq_map:
-            self.freq_map[new_freq] = DoublyLinkedList()
-        
-        self.freq_map[new_freq].add_node(node)
-    
-    def get(self, key: int) -> int:
-        if key not in self.node_map:
-            return -1
-        
-        node = self.node_map[key]
-        self._update_freq(node)
-        return node.value
-    
-    def put(self, key: int, value: int) -> None:
-        if self.capacity == 0:
-            return
-        
-        if key in self.node_map:
-            node = self.node_map[key]
-            node.value = value
-            self._update_freq(node)
-        else:
-            if self.size == self.capacity:
-                lfu_list = self.freq_map[self.min_freq]
-                node_to_remove = lfu_list.remove_last()
-                del self.node_map[node_to_remove.key]
-                self.size -= 1
+        # 2. If this bucket became empty and it was the global minimum frequency, increment min_freq
+        if not self.freq_map[freq] and freq == self.min_freq:
+            self.min_freq += 1
+            del self.freq_map[freq]
             
-            new_node = Node(key, value)
-            self.node_map[key] = new_node
-            self.min_freq = 1
-            if 1 not in self.freq_map:
-                self.freq_map[1] = DoublyLinkedList()
-            self.freq_map[1].add_node(new_node)
-            self.size += 1
+        # 3. Insert the key into the incremented frequency bucket (moves to the end/most recent)
+        new_freq = freq + 1
+        self.freq_map[new_freq][key] = True
+        self.key_map[key] = (curr_val, new_freq)
+        
+        return curr_val
 
+    def get(self, key: int) -> int:
+        if key not in self.key_map:
+            return -1
+        return self._update_frequency(key)
 
+    def put(self, key: int, value: int) -> None:
+        if self.cap == 0:
+            return
 
-# Your LFUCache object will be instantiated and called as such:
-# obj = LFUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        # Scenario A: Key already exists, update its value and elevate its frequency
+        if key in self.key_map:
+            self._update_frequency(key, value)
+            return
+
+        # Scenario B: Cache is full, evict the LFU (and LRU if tied) element first
+        if len(self.key_map) >= self.cap:
+            # The first item in an OrderedDict is the oldest inserted (the tail of our DLL concept)
+            evict_key, _ = self.freq_map[self.min_freq].popitem(last=False)
+            del self.key_map[evict_key]
+            
+            if not self.freq_map[self.min_freq]:
+                del self.freq_map[self.min_freq]
+
+        # Scenario C: Insert the completely new element
+        self.key_map[key] = (value, 1)
+        self.freq_map[1][key] = True
+        self.min_freq = 1  # Reset min_freq to 1 since a brand new item has just entered
