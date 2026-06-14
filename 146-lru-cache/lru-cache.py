4cd5c53456e4b3dc363f1792d42d33e7216c1449
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-14 19:44:56 +0530

    Time: 175 ms (9.83%) | Memory: 76.9 MB (61.10%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,72 +1,55 @@
-class Node:
-    def __init__(self, key: int, value: int):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
-
 class LRUCache:
+    class node:
+        def __init__(self,_key,_value):
+            self.key=_key
+            self.value=_value
+            self.next=None
+            self.prev=None
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = {}  # map key to node
-        self.head = Node(-1, -1)
-        self.tail = Node(-1, -1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.cap=capacity
+        self.mpp={}
+        self.head=self.node(-1,-1)
+        self.tail=self.node(-1,-1)
+        self.head.next=self.tail
+        self.tail.prev=self.head
 
-    def _add_node(self, node: Node):
-        """Add node right after head."""
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
-
-    def _remove_node(self, node: Node):
-        """Remove an existing node from the linked list."""
-        prev_node = node.prev
-        next_node = node.next
-        prev_node.next = next_node
-        next_node.prev = prev_node
-
-    def _move_to_head(self, node: Node):
-        """Move certain node in between to the head."""
-        self._remove_node(node)
-        self._add_node(node)
-
-    def _pop_tail(self) -> Node:
-        """Pop the current tail."""
-        res = self.tail.prev
-        self._remove_node(res)
-        return res
+    def addNode(self,newNode):
+        temp=self.head.next
+        self.head.next=newNode
+        newNode.prev=self.head
+        newNode.next=temp
+        temp.prev=newNode
+        
+    def delNode(self,delnode):
+        delnext=delnode.next
+        delprev=delnode.prev
+        delprev.next=delnext
+        delnext.prev=delprev
 
     def get(self, key: int) -> int:
-        node = self.cache.get(key)
-        if not node:
-            return -1
-        # Move the accessed node to the head
-        self._move_to_head(node)
-        return node.value
+        if key in self.mpp:
+            resNode=self.mpp[key]
+            res=resNode.value
+            del self.mpp[key]
+            self.delNode(resNode)
+            self.addNode(resNode)
+            self.mpp[key]=self.head.next
+            return res
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        node = self.cache.get(key)
+        if key in self.mpp:
+            existingNode=self.mpp[key]
+            del self.mpp[key]
+            self.delNode(existingNode)
+        if len(self.mpp)==self.cap:
+            del self.mpp[self.tail.prev.key]
+            self.delNode(self.tail.prev)
+        self.addNode(self.node(key, value))
+        self.mpp[key] = self.head.next
+            
 
-        if not node:
-            # Create a new node
-            new_node = Node(key, value)
-            self.cache[key] = new_node
-            self._add_node(new_node)
-
-            if len(self.cache) > self.capacity:
-                # Pop the tail
-                tail = self._pop_tail()
-                del self.cache[tail.key]
-        else:
-            # Update the value and move to head
-            node.value = value
-            self._move_to_head(node)
-
-        
 
 
 # Your LRUCache object will be instantiated and called as such:
