ba852e44ed5bfb94d4cd1ff3ddcffb418a477849
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-26 18:48:25 +0530

    Time: 7 ms (14.54%) | Memory: 20.4 MB (50.60%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,20 +1,22 @@
 class Solution:
-    def func(self,ind,prices,buy,dp):
-        if len(prices)==ind:
-            return 0
-        if dp[ind][buy]!=-1: return dp[ind][buy]
-        profit=0
-        if buy:
-            # Option 1: Buy today -> deduct money, next state becomes 'cannot buy' (0)
-            # Option 2: Skip today -> money stays same, next state stays 'can buy' (1)
-            profit=max(-prices[ind]+self.func(ind+1,prices,0,dp),self.func(ind+1,prices,1,dp))
-        else:
-            # Option 1: Sell today -> gain money, next state becomes 'can buy' (1)
-            # Option 2: Skip today -> money stays same, next state stays 'cannot buy' (0)
-            profit=max(prices[ind]+self.func(ind+1,prices,1,dp),self.func(ind+1,prices,0,dp))
-        dp[ind][buy]= profit
-        return dp[ind][buy]
+    def func(self,prices,dp):
+        n=len(prices)
+        curr=[0]*2
+        for ind in range(n-1,-1,-1):
+            for buy in range(2):
+                profit=0
+                if buy:
+                    # Option 1: Buy today -> deduct money, next state becomes 'cannot buy' (0)
+                    # Option 2: Skip today -> money stays same, next state stays 'can buy' (1)
+                    profit=max(-prices[ind]+dp[0],dp[1])
+                else:
+                    # Option 1: Sell today -> gain money, next state becomes 'can buy' (1)
+                    # Option 2: Skip today -> money stays same, next state stays 'cannot buy' (0)
+                    profit=max(prices[ind]+dp[1],dp[0])
+                curr[buy]= profit
+            dp=curr
+        return dp[1]
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[-1]*2 for _ in range(n)]
-        return self.func(0,prices,1,dp)
+        dp=[0]*2 
+        return self.func(prices,dp)
