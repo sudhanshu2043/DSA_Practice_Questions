804b990df7b02710ce7a5caf9ac579fdd3023e6a
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-26 19:10:14 +0530

    Time: 339 ms (29.83%) | Memory: 30.8 MB (71.69%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -3,28 +3,28 @@ from typing import List
 class Solution:
     def func(self, prices, dp):
         n = len(prices)
-        
+        curr = [[0] * 3 for _ in range(2)]
         # Loop backwards from day n-1 down to 0
         for ind in range(n - 1, -1, -1):
             for buy in range(2):
                 # FIX 1: Start from 1 to avoid cap-1 becoming -1
                 for cap in range(1, 3): 
                     if buy:
-                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind + 1][0][cap], 
-                                                 dp[ind + 1][1][cap])
+                        curr[buy][cap] = max(-prices[ind] + dp[0][cap], 
+                                                 dp[1][cap])
                     else:
-                        dp[ind][buy][cap] = max(prices[ind] + dp[ind + 1][1][cap - 1], 
-                                                 dp[ind + 1][0][cap])
-                                    
+                        curr[buy][cap] = max(prices[ind] + dp[1][cap - 1], 
+                                                 dp[0][cap])
+            dp=curr                   
         # FIX 3: Return the specific starting state: Day 0, Can Buy, 2 Transactions left
-        return dp[0][1][2]
+        return dp[1][2]
 
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
         
         # FIX 2: Initialize with 0 instead of -1. 
         # This automatically sets up the base cases for ind == n and cap == 0.
-        dp = [[[0] * 3 for _ in range(2)] for _ in range(n + 1)]
+        dp = [[0] * 3 for _ in range(2)]
         
         # No need to pass starting values as parameters anymore
         return self.func(prices, dp)
