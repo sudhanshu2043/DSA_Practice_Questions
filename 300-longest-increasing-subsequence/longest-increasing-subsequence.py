65b8c4ae04e74d2456283fc497ce207e99e1b049
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-27 11:34:51 +0530

    Time: 7 ms (83.16%) | Memory: 19.4 MB (59.00%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,14 +1,28 @@
 class Solution:
-    def func(self,nums,ind,prev,dp):
-        n=len(nums)
-        for ind in range(n-1,-1,-1):
-            for prev in range(ind-1,-2,-1):
-                ans=dp[ind+1][prev+1] #not take
-                if prev==-1 or nums[ind]>nums[prev]:
-                    ans=max(ans,1+dp[ind+1][ind+1])
-                dp[ind][prev+1]=ans
-        return dp[0][0]
     def lengthOfLIS(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[[0]*(n+1) for _ in range(n+1)]
-        return self.func(nums,0,-1,dp)
+        n = len(nums)
+        temp = []
+        temp.append(nums[0])
+        # Iterate on the elements of the array
+        for i in range(1, n):
+            # If the current element can be added to the subsequence
+            if nums[i] > temp[-1]:
+                temp.append(nums[i]) 
+            # Otherwise
+            else:
+                # Index at which the current element must be placed
+                ind = self.lower_bound(temp, nums[i])
+                # Place the current element in the subsequence
+                temp[ind] = nums[i]
+        # Return the length of temporary subsequence created so far
+        return len(temp)
+    # Binary search to find the index of the smallest element >= target
+    def lower_bound(self, temp, target):
+        left, right = 0, len(temp) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if temp[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
