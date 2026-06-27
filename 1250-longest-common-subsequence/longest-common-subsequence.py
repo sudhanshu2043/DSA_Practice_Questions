47739e0f8cc364b05bd1badaa517beb8fed1a7ee
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-27 18:18:12 +0530

    Time: 600 ms (25.83%) | Memory: 27.7 MB (84.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,26 +1,15 @@
 class Solution:
-    def func(self,i,j,text1,text2,dp):
-        if i==-1 or j==-1: return 0
-        if dp[i][j]!=-1: return dp[i][j]
-        if text1[i]==text2[j]:
-            dp[i][j]=1+self.func(i-1,j-1,text1,text2,dp)
-            return dp[i][j]
-        dp[i][j]=max(self.func(i-1,j,text1,text2,dp),self.func(i,j-1,text1,text2,dp))
-        return dp[i][j]
+    def func(self,s1,s2,ind1,ind2,dp):
+        if ind1<0 or ind2<0: return 0
+        if dp[ind1][ind2]!=-1:
+            return dp[ind1][ind2]
+        #match
+        if s1[ind1]==s2[ind2]:
+            dp[ind1][ind2]= 1+self.func(s1,s2,ind1-1,ind2-1,dp)
+        else:
+            dp[ind1][ind2]= max(self.func(s1,s2,ind1,ind2-1,dp),self.func(s1,s2,ind1-1,ind2,dp))
+        return dp[ind1][ind2]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        n=len(text1)
-        m=len(text2)
-        dp=[[-1]*(m+1) for _ in range(n+1)]
-        for i in range(n+1):
-            dp[i][0]=0
-        for j in range(m+1):
-            dp[0][j]=0
-        # Base case
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if text1[i-1]==text2[j-1]:
-                    dp[i][j]=1+dp[i-1][j-1]
-                else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[n][m]
-        
+        n,m=len(text1),len(text2)
+        dp=[[-1]*m for _ in range(n)]
+        return self.func(text1,text2,len(text1)-1,len(text2)-1,dp)
