d396e15ef454f2090a8ace2d949b9493901475bc
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-27 22:11:26 +0530

    Time: 414 ms (54.33%) | Memory: 75.7 MB (50.54%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -1,14 +1,16 @@
 class Solution:
-    def func(self,s,t,i,j,dp):
-        if j<0: return 1
-        if i<0: return 0
-        if dp[i][j]!=-1: return dp[i][j]
-        if s[i]==t[j]:
-            dp[i][j] =(self.func(s,t,i-1,j-1,dp)+self.func(s,t,i-1,j,dp))
-        else:
-            dp[i][j] = self.func(s,t,i-1,j,dp)
-        return dp[i][j]
+    def func(self,s,t,dp):
+        n,m=len(s),len(t)
+        for i in range(len(s)):
+            dp[i][0]=1
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if s[i-1]==t[j-1]:
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j]
+        return dp[n][m]
     def numDistinct(self, s: str, t: str) -> int:
         n,m=len(s),len(t)
-        dp=[[-1]*m for _ in range(n)]
-        return self.func(s,t,len(s)-1,len(t)-1,dp)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        return self.func(s,t,dp)
