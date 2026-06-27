f9a6000596dc0ebb70840186320bc0d84d41a245
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-27 18:31:43 +0530

    Time: 339 ms (78.15%) | Memory: 44.3 MB (73.80%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,15 +1,16 @@
 class Solution:
-    def func(self,s1,s2,ind1,ind2,dp):
-        if ind1<0 or ind2<0: return 0
-        if dp[ind1][ind2]!=-1:
-            return dp[ind1][ind2]
-        #match
-        if s1[ind1]==s2[ind2]:
-            dp[ind1][ind2]= 1+self.func(s1,s2,ind1-1,ind2-1,dp)
-        else:
-            dp[ind1][ind2]= max(self.func(s1,s2,ind1,ind2-1,dp),self.func(s1,s2,ind1-1,ind2,dp))
-        return dp[ind1][ind2]
+    def func(self,s1,s2,dp):
+        n = len(s1)
+        m = len(s2)
+        for ind1 in range(1,n+1):
+            for ind2 in range(1,m+1):
+                #match
+                if s1[ind1-1]==s2[ind2-1]:
+                    dp[ind1][ind2]= 1+dp[ind1-1][ind2-1]
+                else:
+                    dp[ind1][ind2]= max(dp[ind1][ind2-1],dp[ind1-1][ind2])
+        return dp[n][m]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         n,m=len(text1),len(text2)
-        dp=[[-1]*m for _ in range(n)]
-        return self.func(text1,text2,len(text1)-1,len(text2)-1,dp)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        return self.func(text1,text2,dp)
