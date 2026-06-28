0aa5803fe5b6a08c6fa820dbfa26f204fcde75fd
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-28 11:58:02 +0530

    Time: 2624 ms (14.45%) | Memory: 19.4 MB (73.88%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.py
@@ -2,27 +2,22 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         return s == s[::-1]
 
-    def func(self, i, s, dp):
-        if i == len(s): 
-            return 0
-        
-        if dp[i] != -1:
-            return dp[i]
-            
-        minCost = 1e9
-        temp = ""
-        for j in range(i, len(s)):
-            temp += s[j]
-            if self.isPalindrome(temp):
-                # Count blocks instead of cuts to avoid boundary edge cases
-                cost = 1 + self.func(j + 1, s, dp)
-                minCost = min(minCost, cost)
-                
-        dp[i] = minCost
-        return dp[i]
+    def func(self, s, dp):
+        for i in range(len(s)-1,-1,-1):
+            minCost = 1e9
+            temp = ""
+            for j in range(i, len(s)):
+                temp += s[j]
+                if self.isPalindrome(temp):
+                    # Count blocks instead of cuts to avoid boundary edge cases
+                    cost = 1 + dp[j + 1]
+                    minCost = min(minCost, cost)
+                    
+            dp[i] = minCost
+        return dp[0]
 
     def minCut(self, s: str) -> int:
         n = len(s)
-        dp = [-1] * n
+        dp = [0] * (n+1)
         # Subtract 1 at the end because cuts = total_parts - 1
-        return self.func(0, s, dp) - 1
+        return self.func( s, dp) - 1
