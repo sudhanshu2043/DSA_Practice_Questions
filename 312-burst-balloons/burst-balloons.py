ad91508ca09074da87905fc3b49ef8ae47046eca
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-06-28 11:06:50 +0530

    Time: 2085 ms (70.86%) | Memory: 22.2 MB (65.45%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -1,15 +1,17 @@
 class Solution:
     def func(self,nums,i,j,dp):
-        if i>j: return 0
-        maxi=-1e9
-        if dp[i][j]!=-1: return dp[i][j]
-        for ind in range(i,j+1):
-            coin=(nums[i-1]*nums[ind]*nums[j+1])+self.func(nums,i,ind-1,dp)+self.func(nums,ind+1,j,dp)
-            maxi=max(maxi,coin)
-        dp[i][j]=maxi
-        return dp[i][j]
+        m=len(nums)
+        for i in range(m-2,0,-1):
+            for j in range(i,m-1):
+                maxi=-1e9
+                if i>j: continue
+                for ind in range(i,j+1):
+                    coin=(nums[i-1]*nums[ind]*nums[j+1])+dp[i][ind-1]+dp[ind+1][j]
+                    maxi=max(maxi,coin)
+                dp[i][j]=maxi
+        return dp[1][m-2]
     def maxCoins(self, nums: List[int]) -> int:
         nums=[1]+nums+[1]
         m=len(nums)
-        dp=[[-1]*m for _ in range(m)]
+        dp=[[0]*m for _ in range(m)]
         return self.func(nums,1,m-2,dp)
