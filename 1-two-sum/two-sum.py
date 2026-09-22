e4dcc18245e42d7aa6ecadc7e51c458fc2d81ac5
Sudhanshu Kumar <65844744+sudhanshu2043@users.noreply.github.com>
2026-09-22 23:00:57 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (59.43%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        temp={}
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        map={}
+        ans=[]
         for i in range(len(nums)):
-            temp[nums[i]]=i
-        for i in range(len(nums)):
-            more_needed=target-nums[i]
-            if more_needed in temp.keys() and i!=temp.get(more_needed):
-                return [i,temp.get(more_needed)]
-        return [-1,-1]
+            if target-nums[i] in map:
+                return [map[target-nums[i]],i]
+            else:
+                map[nums[i]]=i
+        return []
